f56a1cdd19c1a74dde88890b2675201e7d741f6d
Trần Hữu An <tranhuutukkt@gmail.com>
2022-06-09 01:18:18 +0700

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,7 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+    const [items, putItems, clearItems] = useStorage();
 
     const [filter, setFilter] = React.useState('ALL');
 
@@ -71,6 +65,10 @@ function Todo() {
         <div className="panel-block">
             {displayItems.length} items
         </div>
+        <button
+            className="button is-danger"
+            onClick={clearItems}
+        >全てのToDoを削除</button>
     </div>
   );
 }
