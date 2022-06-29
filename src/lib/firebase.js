82be44182df6b684aa439fc9a0472538aea2a85c
Trần Hữu An <tranhuutukkt@gmail.com>
2022-06-29 21:51:34 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJr6jr6QbLTA5IkJ_JpvklRVeOBqbdR9U",
+    authDomain: "itss-firebase-217d0.firebaseapp.com",
+    projectId: "itss-firebase-217d0",
+    storageBucket: "itss-firebase-217d0.appspot.com",
+    messagingSenderId: "685926460845",
+    appId: "1:685926460845:web:be4e9c776ae1d9f17d0524"
+};
+
+firebase.initializeApp(firebaseConfig);
